b8e5cc0cd90cdaa8b8b8f7d210169732cb56c5bc
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-07-23 21:57:44 +0200

    fix(search): implement get venues search

--- src/reducers/life.ts
+++ src/reducers/life.ts
@@ -2,9 +2,9 @@ import { NAction } from '../../types/action'
 import { NLife } from '../../types/life'
 import { EActionRequest } from '../actions/life'
 
-export const initialState = {
+export const initialState: NLife.ILife = {
   life: false,
-} as NLife.ILife
+}
 
 export const lifeReducer = (action: NAction.IAction) => {
   switch (action.type) {
---
+++ types/venues.d.ts
@@ -0,0 +1,5 @@
+export declare namespace NVenues {
+  interface IVenues {
+    [key: string]: any
+  }
+}
