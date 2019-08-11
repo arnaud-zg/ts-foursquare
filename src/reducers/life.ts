79ad9dde907d30236423aaaf85665908a384d95b
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-11 22:36:09 +0200

    feat(venue-selector): add venue categories selector

--- src/reducers/life.ts
+++ src/reducers/life.ts
@@ -2,11 +2,11 @@ import { createReducer } from 'typesafe-actions'
 import { NLife } from '../../types/life'
 import { ELifeActionResolve, TLifeActions } from '../actions/life'
 
-export const initialState: NLife.ILifeState = {
+export const initialState: NLife.IState = {
   life: false,
 }
 
-export const lifeReducer = createReducer<NLife.ILifeState, TLifeActions>(
+export const lifeReducer = createReducer<NLife.IState, TLifeActions>(
   initialState,
   {
     [ELifeActionResolve.RESOLVE_GET_LIFE]: (state, action) => ({
---
+++ src/selectors/venues.ts
@@ -0,0 +1,11 @@
+import { TRootState } from '../reducers'
+
+export const venuesSelector = (state: TRootState) => state.venues
+
+export const venueSelector = (state: TRootState, venueId: string) =>
+  venuesSelector(state)[venueId]
+
+export const venueCategoriesStateSelector = (
+  state: TRootState,
+  venueId: string
+) => venuesSelector(state)[venueId].categories
