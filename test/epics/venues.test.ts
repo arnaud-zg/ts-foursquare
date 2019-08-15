4d73c14873788aaf45b9f09fce5af5b4dc68e54d
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-15 23:38:16 +0200

    chore(test): add breakline

--- test/epics/venues.test.ts
+++ test/epics/venues.test.ts
@@ -15,12 +15,36 @@ describe('epics/venues', () => {
   })
 
   test.each`
-    scenario                                                                  | action                                                                                                  | expectedActions
-    ${'should get search for venues by location'}                             | ${requestGetVenuesSearch({ ll: '40.7099,-73.9622' })}                                                   | ${[{ payload: payload.response.venues, type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH }]}
-    ${'should get search for venues by location with intent match parameter'} | ${requestGetVenuesSearch({ ll: '40.7099,-73.9622', intent: 'match' })}                                  | ${[{ payload: payload.response.venues, type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH }]}
-    ${'should get search for venues by query'}                                | ${requestGetVenuesSearch({ query: 'ShopMart' })}                                                        | ${[{ payload: payload.response.venues, type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH }]}
-    ${'should get search for venues near to a location'}                      | ${requestGetVenuesSearch({ near: 'Chicago, IL' })}                                                      | ${[{ payload: payload.response.venues, type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH }]}
-    ${'should get search for venues'}                                         | ${requestGetVenuesSearch({ ll: '40.7099,-73.9622', query: 'ShopMart', intent: 'browse', radius: 500 })} | ${[{ payload: payload.response.venues, type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH }]}
+    scenario | action | expectedActions
+    ${'should get search for venues by location'} | ${requestGetVenuesSearch({ ll: '40.7099,-73.9622' })} | ${[{
+    payload: payload.response.venues,
+    type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH,
+  }]}
+    ${'should get search for venues by location with intent match parameter'} | ${requestGetVenuesSearch({
+  ll: '40.7099,-73.9622',
+  intent: 'match',
+})} | ${[{
+    payload: payload.response.venues,
+    type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH,
+  }]}
+    ${'should get search for venues by query'} | ${requestGetVenuesSearch({
+  query: 'ShopMart',
+})} | ${[{
+    payload: payload.response.venues,
+    type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH,
+  }]}
+    ${'should get search for venues near to a location'} | ${requestGetVenuesSearch({
+  near: 'Chicago, IL',
+})} | ${[{
+    payload: payload.response.venues,
+    type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH,
+  }]}
+    ${'should get search for venues'} | ${requestGetVenuesSearch({
+  ll: '40.7099,-73.9622',
+  query: 'ShopMart',
+  intent: 'browse',
+  radius: 500,
+})} | ${[{ payload: payload.response.venues, type: EVenuesAction.RESOLVE_GET_VENUES_SEARCH }]}
   `('$scenario', ({ action, expectedActions, done }) => {
     testEpic(
       getVenuesSearchEpic,
