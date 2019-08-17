f2eb54f38becff15a268837a6472d303ad62371c
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-18 00:12:00 +0200

    fix(action): add actions index

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,2 +1,7 @@
+import { TLifeActions } from './life'
+import { TVenuesAction } from './venues'
+
+export type TAction = TVenuesAction | TLifeActions
+
 export * from './life'
 export * from './venues'
