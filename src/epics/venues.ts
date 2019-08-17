5a7e111c8dd854c461dfb405d5ec421f387becd1
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-18 00:13:05 +0200

    fix(middlewares): add epic middlewares

--- src/epics/venues.ts
+++ src/epics/venues.ts
@@ -1,20 +1,16 @@
 import { Epic } from 'redux-observable'
 import { filter, map, switchMap } from 'rxjs/operators'
 import { isOfType } from 'typesafe-actions'
-import {
-  EVenuesAction,
-  resolveGetVenuesSearch,
-  TVenuesAction,
-} from '../actions/venues'
+import { NRoot } from '../../types/root.d'
+import { EVenuesAction, resolveGetVenuesSearch } from '../actions/venues'
+import { TAction } from '../actions'
 import { adaptGetVenuesSearch } from '../adapter/venues'
-import { TRootState } from '../reducers'
 import { getObservableVenuesSearch } from '../services/venues'
 
-export const getVenuesSearchEpic: Epic<
-  TVenuesAction,
-  TVenuesAction,
-  TRootState
-> = (action$, state$) =>
+export const getVenuesSearchEpic: Epic<TAction, TAction, NRoot.IState> = (
+  action$,
+  state$
+) =>
   action$.pipe(
     filter(isOfType(EVenuesAction.REQUEST_GET_VENUES_SEARCH)),
     switchMap(action =>
