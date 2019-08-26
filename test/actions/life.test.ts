803756fe984859cd16247d6dc547cf0fda1a0d9b
Arnaud ZHENG <arnaud-zg@users.noreply.github.com>
2019-08-26 14:07:43 +0200

    test(actions): add snapshot on ELifeAction

--- test/actions/life.test.ts
+++ test/actions/life.test.ts
@@ -1,6 +1,10 @@
 import { getLife, ELifeAction } from '../../src/actions/life'
 
 describe('Life', () => {
+  it(`should make a snapshot of ELifeAction`, () => {
+    expect(ELifeAction).toMatchSnapshot()
+  })
+
   it(`should return an action with type ${ELifeAction.RESOLVE_GET_LIFE}`, () => {
     expect(getLife()).toMatchSnapshot()
   })
