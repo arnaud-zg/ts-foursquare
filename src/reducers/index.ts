e0689679f7a5718b9692151adafd5e930d067b8d
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-27 21:49:06 +0200

    feat(dev-tools): Support redux devtools extension

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,13 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { epicMiddleware, rootEpic } from '../middlewares'
 import { appReducer } from './app'
 
 export { initialState } from './app'
 
+const composeEnhancers = composeWithDevTools({})
+
 export const configureStore = () => {
   const store = createStore(
     appReducer,
-    compose(applyMiddleware(epicMiddleware))
+    composeEnhancers(applyMiddleware(epicMiddleware))
   )
 
   epicMiddleware.run(rootEpic)
