d0e38c1508c68391b29014cccbb8d1140f156c48
Arnaud ZHENG <arnaud-zg@users.noreply.github.com>
2019-08-30 18:42:05 +0200

    chore(type): import TRootAction

--- src/epics/venues.ts
+++ src/epics/venues.ts
@@ -3,14 +3,15 @@ import { filter, map, switchMap } from 'rxjs/operators'
 import { isOfType } from 'typesafe-actions'
 import { NRoot } from '../../types/root.d'
 import { EVenuesAction, resolveGetVenuesSearch } from '../actions/venues'
-import { TAction } from '../actions'
+import { TRootAction } from '../actions'
 import { adaptGetVenuesSearch } from '../adapter/venues'
 import { getObservableVenuesSearch } from '../services/venues'
 
-export const getVenuesSearchEpic: Epic<TAction, TAction, NRoot.IState> = (
-  action$,
-  state$
-) =>
+export const getVenuesSearchEpic: Epic<
+  TRootAction,
+  TRootAction,
+  NRoot.IState
+> = (action$, state$) =>
   action$.pipe(
     filter(isOfType(EVenuesAction.REQUEST_GET_VENUES_SEARCH)),
     switchMap(action =>
