a80cdc7732783e2473b9698133331a147cbd0d73
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-30 23:02:51 +0200

    chore(life): remove useless get life action

--- test/actions/life.test.ts
+++ test/actions/life.test.ts
@@ -1,11 +1,7 @@
-import { getLife, ELifeAction } from '../../src/actions/life'
+import { ELifeAction } from '../../src/actions/life'
 
 describe('actions/life', () => {
   it(`should make a snapshot of ELifeAction`, () => {
     expect(ELifeAction).toMatchSnapshot()
-  })
-
-  it(`should return an action with type ${ELifeAction.RESOLVE_GET_LIFE}`, () => {
-    expect(getLife()).toMatchSnapshot()
   })
 })
--- test/reducers/life.test.ts
+++ test/reducers/life.test.ts
@@ -1,10 +1,9 @@
-import { getLife, putCredentials } from '../../src/actions/life'
+import { putCredentials } from '../../src/actions/life'
 import { lifeReducer, initialState } from '../../src/reducers/life'
 
 describe('reducers/life', () => {
   test.each`
     scenario                                           | action
-    ${'should get state after action: getLife'}        | ${getLife()}
     ${'should get state after action: putCredentials'} | ${putCredentials({ clientId: '1234', clientSecret: '5678' })}
   `('$scenario with action: $action', ({ action }) => {
     expect(lifeReducer(initialState, action)).toMatchSnapshot()
