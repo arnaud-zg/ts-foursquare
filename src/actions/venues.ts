dcaca7fac49e70c6cab0c017de292125eecf4835
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-01 20:47:13 +0200

    fix(venues): create async action for venues epic

--- src/actions/venues.ts
+++ src/actions/venues.ts
@@ -1,11 +1,23 @@
-import { action, ActionType } from 'typesafe-actions'
+import { action, ActionType, createAsyncAction } from 'typesafe-actions'
 import { NVenue } from '../../types/venue.d'
 import { NRequest } from '../../types/request.d'
 
 export enum EVenuesAction {
   REQUEST_GET_VENUES_SEARCH = 'REQUEST_GET_VENUES_SEARCH',
   RESOLVE_GET_VENUES_SEARCH = 'RESOLVE_GET_VENUES_SEARCH',
+  // getVenuesSearchAsync
+  GET_VENUES_SEARCH_REQUEST = 'GET_VENUES_SEARCH_REQUEST',
+  GET_VENUES_SEARCH_SUCCESS = 'GET_VENUES_SEARCH_SUCCESS',
+  GET_VENUES_SEARCH_FAILURE = 'GET_VENUES_SEARCH_FAILURE',
+  GET_VENUES_SEARCH_CANCEL = 'GET_VENUES_SEARCH_CANCEL',
 }
+
+export const getVenuesSearchAsync = createAsyncAction(
+  'GET_VENUES_SEARCH_REQUEST',
+  'GET_VENUES_SEARCH_SUCCESS',
+  'GET_VENUES_SEARCH_FAILURE',
+  'GET_VENUES_SEARCH_CANCEL'
+)<string, NVenue.IVenue[], Error, string>()
 
 export const requestGetVenuesSearch = (
   payload: NRequest.TVenuesSearchPayload
@@ -17,6 +29,7 @@ export const resolveGetVenuesSearch = (payload: NVenue.IVenue[] = []) =>
 export const actions = {
   requestGetVenuesSearch,
   resolveGetVenuesSearch,
+  getVenuesSearchAsync,
 }
 
 export type TVenuesAction = ActionType<typeof actions>
