0e1e87f46fff7450bddcbbe553946953b11df5bd
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-03 23:09:54 +0200

    feat(status): add status module

--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux'
 import { lifeReducer } from './life'
 import { venuesReducer } from './venues'
+import { statusReducer } from './status'
 import { initialState as initialStateLife } from '../../src/reducers/life'
+import { initialState as initialStateStatus } from '../../src/reducers/status'
 import { initialState as initialStateVenues } from '../../src/reducers/venues'
 
 export const initialState = {
   life: initialStateLife,
+  status: initialStateStatus,
   venues: initialStateVenues,
 }
 
@@ -13,5 +16,6 @@ export type TState = typeof initialState
 
 export const appReducer = combineReducers({
   life: lifeReducer,
+  status: statusReducer,
   venues: venuesReducer,
 })
