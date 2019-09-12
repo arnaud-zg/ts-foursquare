2fdbe08b88f437d9fc1ef9c2f0584bf74eecf0fa
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-12 10:00:51 +0200

    chore(package): remove adapter, epics and services from package (#178)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 require('isomorphic-fetch')
 
 export * from './actions'
-export * from './adapter'
-export * from './epics'
 export * from './reducers'
 export * from './selectors'
-export * from './services'
 export * from './utils'
