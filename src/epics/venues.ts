4f7fbda58fa99195a767e0b98897ea7c8a8f3df9
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-13 22:53:33 +0200

    feat(venue-explore): create an epic venue explore

--- src/epics/venues.ts
+++ src/epics/venues.ts
@@ -4,9 +4,16 @@ import { catchError, filter, map, switchMap, takeUntil } from 'rxjs/operators'
 import { isOfType } from 'typesafe-actions'
 import { NStore } from '../../types/store'
 import { TRootAction } from '../actions'
-import { EVenuesAction, getVenuesSearchAsync } from '../actions/venues'
-import { adaptGetVenuesSearch } from '../adapter/venues'
-import { getObservableVenuesSearch } from '../services/venues'
+import {
+  EVenuesAction,
+  getVenuesSearchAsync,
+  getVenueExploreAsync,
+} from '../actions/venues'
+import { adaptGetVenuesSearch, adaptGetVenuesExplore } from '../adapter/venues'
+import {
+  getObservableVenuesSearch,
+  getObservableVenuesExplore,
+} from '../services/venues'
 
 export const getVenuesSearchEpic: Epic<
   TRootAction,
@@ -22,6 +29,27 @@ export const getVenuesSearchEpic: Epic<
         catchError(err => of(getVenuesSearchAsync.failure(err))),
         takeUntil(
           action$.pipe(filter(isOfType(EVenuesAction.GET_VENUES_SEARCH_CANCEL)))
+        )
+      )
+    )
+  )
+
+export const getVenuesExploreEpic: Epic<
+  TRootAction,
+  TRootAction,
+  NStore.IState
+> = (action$, state$) =>
+  action$.pipe(
+    filter(isOfType(EVenuesAction.GET_VENUES_EXPLORE_REQUEST)),
+    switchMap(action =>
+      getObservableVenuesExplore(action, state$).pipe(
+        map(adaptGetVenuesExplore),
+        map(getVenueExploreAsync.success),
+        catchError(err => of(getVenuesSearchAsync.failure(err))),
+        takeUntil(
+          action$.pipe(
+            filter(isOfType(EVenuesAction.GET_VENUES_EXPLORE_CANCEL))
+          )
         )
       )
     )
--- test/epics/venues.test.ts
+++ test/epics/venues.test.ts
@@ -1,6 +1,12 @@
 import { Action } from 'typesafe-actions'
-import { getVenuesSearchAsync } from '../../src/actions/venues'
-import { adaptGetVenuesSearch } from '../../src/adapter/venues'
+import {
+  getVenueExploreAsync,
+  getVenuesSearchAsync,
+} from '../../src/actions/venues'
+import {
+  adaptGetVenuesExplore,
+  adaptGetVenuesSearch,
+} from '../../src/adapter/venues'
 import { getVenuesSearchEpic } from '../../src/epics/venues'
 import { initialState } from '../../src/reducers/app'
 import {
@@ -9,22 +15,21 @@ import {
   mockingFetch,
   testEpic,
 } from '../../src/utils/test'
-import { payload } from './__mocks__/getVenuesSearchAsync.resolve'
+import { payload as payloadGetVenuesExplore } from './__mocks__/getVenuesExploreAsync.resolve'
+import { payload as payloadGetVenuesSearch } from './__mocks__/getVenuesSearchAsync.resolve'
 require('isomorphic-fetch')
 
 describe('epics/venues', () => {
-  beforeAll(() => {
-    mockingFetch({ response: { ...payload } })
-  })
-
   test.each`
-    scenario                                                                  | action                                                                                                        | expectedActions
-    ${'should get search for venues by location'}                             | ${getVenuesSearchAsync.request({ ll: '40.7099,-73.9622' })}                                                   | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payload))]}
-    ${'should get search for venues by location with intent match parameter'} | ${getVenuesSearchAsync.request({ intent: 'match', ll: '40.7099,-73.9622' })}                                  | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payload))]}
-    ${'should get search for venues by query'}                                | ${getVenuesSearchAsync.request({ query: 'ShopMart' })}                                                        | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payload))]}
-    ${'should get search for venues near to a location'}                      | ${getVenuesSearchAsync.request({ near: 'Chicago, IL' })}                                                      | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payload))]}
-    ${'should get search for venues'}                                         | ${getVenuesSearchAsync.request({ intent: 'browse', ll: '40.7099,-73.9622', query: 'ShopMart', radius: 500 })} | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payload))]}
-  `('$scenario', ({ action, expectedActions, done }) => {
+    scenario                                                                  | action                                                                                                        | payload                    | expectedActions
+    ${'should get search for venues by location'}                             | ${getVenuesSearchAsync.request({ ll: '40.7099,-73.9622' })}                                                   | ${payloadGetVenuesSearch}  | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payloadGetVenuesSearch))]}
+    ${'should get search for venues by location with intent match parameter'} | ${getVenuesSearchAsync.request({ intent: 'match', ll: '40.7099,-73.9622' })}                                  | ${payloadGetVenuesSearch}  | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payloadGetVenuesSearch))]}
+    ${'should get search for venues by query'}                                | ${getVenuesSearchAsync.request({ query: 'ShopMart' })}                                                        | ${payloadGetVenuesSearch}  | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payloadGetVenuesSearch))]}
+    ${'should get search for venues near to a location'}                      | ${getVenuesSearchAsync.request({ near: 'Chicago, IL' })}                                                      | ${payloadGetVenuesSearch}  | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payloadGetVenuesSearch))]}
+    ${'should get search for venues'}                                         | ${getVenuesSearchAsync.request({ intent: 'browse', ll: '40.7099,-73.9622', query: 'ShopMart', radius: 500 })} | ${payloadGetVenuesSearch}  | ${[getVenuesSearchAsync.success(adaptGetVenuesSearch(payloadGetVenuesSearch))]}
+    ${'should get recommended places'}                                        | ${getVenueExploreAsync.request({})}                                                                           | ${payloadGetVenuesExplore} | ${[getVenueExploreAsync.success(adaptGetVenuesExplore(payloadGetVenuesExplore))]}
+  `('$scenario', ({ action, expectedActions, payload, done }) => {
+    mockingFetch({ response: { ...payload } })
     testEpic(
       getVenuesSearchEpic,
       expectedActions.length,
@@ -49,6 +54,7 @@ describe('epics/venues | error case: no network', () => {
   test.each`
     scenario                                      | action
     ${'should get search for venues by location'} | ${getVenuesSearchAsync.request({ ll: '40.7099,-73.9622' })}
+    ${'should get search for venues by location'} | ${getVenueExploreAsync.request({})}
   `('$scenario', ({ action, done }) => {
     testEpic(
       getVenuesSearchEpic,
@@ -71,6 +77,7 @@ describe('epics/venues | error case: not valid response', () => {
   test.each`
     scenario                                      | action
     ${'should get search for venues by location'} | ${getVenuesSearchAsync.request({ ll: '40.7099,-73.9622' })}
+    ${'should get search for venues by location'} | ${getVenueExploreAsync.request({})}
   `('$scenario', ({ action, done }) => {
     testEpic(
       getVenuesSearchEpic,
