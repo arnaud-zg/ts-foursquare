5ca978daa4b1ba7dcb6ab1ed6ced3ad6231f059e
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-23 22:17:31 +0200

    refactor(life-action): avoid use life action string constant

--- src/actions/life.ts
+++ src/actions/life.ts
@@ -1,12 +1,10 @@
-import { action, ActionType } from 'typesafe-actions'
+import { createAction, ActionType } from 'typesafe-actions'
 import { NResolve } from '../../types/resolve'
 
-export enum ELifeAction {
-  PUT_CREDENTIALS = 'PUT_CREDENTIALS',
-}
-
-export const putCredentials = (payload: NResolve.ICredentialsPayload) =>
-  action(ELifeAction.PUT_CREDENTIALS, payload)
+export const putCredentials = createAction(
+  'PUT_CREDENTIALS',
+  action => (payload: NResolve.ICredentialsPayload) => action(payload)
+)
 
 const actions = {
   putCredentials,
