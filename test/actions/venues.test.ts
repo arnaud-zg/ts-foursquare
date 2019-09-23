5a622d2349419a91c9ddfb9de84afb7f40fade3a
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-23 22:59:03 +0200

    test(venue): update test on venue action

--- test/actions/venues.test.ts
+++ test/actions/venues.test.ts
@@ -1,8 +1,8 @@
-import { EVenuesAction } from '../../src/actions/venues'
+import { venuesActions } from '../../src/actions/venues'
 require('isomorphic-fetch')
 
 describe('actions/venues', () => {
-  it('should make a snapshot of EVenuesAction', () => {
-    expect(EVenuesAction).toMatchSnapshot()
+  it('should make a snapshot of venuesActions', () => {
+    expect(venuesActions).toMatchSnapshot()
   })
 })
