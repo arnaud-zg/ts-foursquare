e3f02bb70aee8f3227f169afc4f9a49593b055c9
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-02 22:12:21 +0200

    feat(venue-suggest-completion): add an adapter for venue suggest completion (#243)

---
+++ test/epics/__mocks__/getVenuesLikesAsync.resolve.ts
@@ -0,0 +1,46 @@
+export const payload = {
+  meta: {
+    code: 200,
+    requestId: '5d950254cc7d41002b479dd8',
+  },
+  response: {
+    likes: {
+      count: 24,
+      summary: '24 Likes',
+      items: [
+        {
+          id: '66019143',
+          firstName: 'Joe',
+          lastName: 'Benz',
+          gender: 'male',
+          photo: {
+            prefix: 'https://fastly.4sqi.net/img/user/',
+            suffix:
+              '/66019143_U7cpUhMp_MBPhsA_er-CfMmMiFXj2KsurXDZxMKR9KSiqf8vDKNtm8CuETX01F_RUzfyzjpa6.jpg',
+          },
+        },
+        {
+          id: '275151',
+          firstName: 'Tyler',
+          lastName: 'Elliott',
+          gender: 'male',
+          photo: {
+            prefix: 'https://fastly.4sqi.net/img/user/',
+            suffix: '/275151-4G0WJJPRVXOCJBAS.jpg',
+          },
+        },
+        {
+          id: '29109434',
+          firstName: 'Miraculous',
+          lastName: 'Assembly',
+          gender: 'none',
+          photo: {
+            prefix: 'https://fastly.4sqi.net/img/user/',
+            suffix:
+              '/29109434_EKGvTLjo_d3wFukA0xdHXaFwxCMmxzb3BoI_6W1JQVeXLs3rgbsD3tGOPhAZoY8-wnUghn_LP',
+          },
+        },
+      ],
+    },
+  },
+}
