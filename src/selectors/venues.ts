dadf122f0f24f9c91db4f03658d79cc60f15725b
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-07 23:29:16 +0200

    refactor(selectors): reorder venues selectors (#251)

--- src/selectors/venues.ts
+++ src/selectors/venues.ts
@@ -1,20 +1,5 @@
 import { NStore } from '../../types/store'
 
-export const venuesSelector = (state: NStore.IState) => state.venues
-
-export const venuesEntitiesSelector = (state: NStore.IState) =>
-  venuesSelector(state).entities
-
-export const venuesRecommendedPlacesSelector = (state: NStore.IState) => {
-  return venuesSelector(state).recommendedPlaces || []
-}
-
-export const venuesCategoriesSelector = (state: NStore.IState) =>
-  venuesSelector(state).categories || []
-
-export const venueSelector = (state: NStore.IState, venueId: string) =>
-  venuesEntitiesSelector(state)[venueId]
-
 export const venueCategoriesStateSelector = (
   state: NStore.IState,
   venueId: string
@@ -22,3 +7,17 @@ export const venueCategoriesStateSelector = (
 
 export const venueLocationSelector = (state: NStore.IState, venueId: string) =>
   venuesEntitiesSelector(state)[venueId].location
+
+export const venuesCategoriesSelector = (state: NStore.IState) =>
+  venuesSelector(state).categories || []
+
+export const venueSelector = (state: NStore.IState, venueId: string) =>
+  venuesEntitiesSelector(state)[venueId]
+
+export const venuesEntitiesSelector = (state: NStore.IState) =>
+  venuesSelector(state).entities
+
+export const venuesRecommendedPlacesSelector = (state: NStore.IState) =>
+  venuesSelector(state).recommendedPlaces || []
+
+export const venuesSelector = (state: NStore.IState) => state.venues
