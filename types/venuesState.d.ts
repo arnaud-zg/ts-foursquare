27772ef61e7ab8429f6505d512d0edd75c71acca
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-23 23:17:53 +0200

    feat(suggest-completion): add mini venues in state (#309)

--- types/venuesState.d.ts
+++ types/venuesState.d.ts
@@ -1,4 +1,5 @@
 import { NLikes } from './likes'
+import { NMiniVenue } from './miniVenue'
 import { NRecommendedPlaces } from './recommendedPlaces'
 import { NVenue } from './venue'
 import { NVenuesCategories } from './venuesCategories'
@@ -8,6 +9,7 @@ export declare namespace NVenues {
     categories: { [key: string]: NVenuesCategories.ICategory }
     entities: { [key: string]: NVenue.IVenue }
     likesEntities: { [key: string]: NLikes.IItem }
+    miniVenues: { [key: string]: NMiniVenue.IMiniVenue }
     nextVenues: { [key: string]: NVenue.IVenue }
     recommendedPlaces: NRecommendedPlaces.IGroupItem[]
     similarVenues: { [key: string]: NVenue.IVenue }
