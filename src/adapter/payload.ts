25dc799504134b2071aabbd8fe55f3b40a0a11fc
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-11-01 13:35:13 +0100

    fix(http): accept all http code begin with 2xx

--- src/adapter/payload.ts
+++ src/adapter/payload.ts
@@ -1,9 +1,12 @@
-import { NHttpStatuses } from 'http-response-status'
+import {
+  getHttpStatusCategory,
+  HTTP_STATUS_SUCCESS,
+} from 'http-response-status'
 import { NPayload } from '../../types'
 
 export const adaptPayload = <TResponse>(
   payload: NPayload.IPayload<TResponse>
 ) =>
   !!payload.meta &&
-  payload.meta.code === NHttpStatuses.ESuccess.OK &&
+  getHttpStatusCategory(payload.meta.code) === HTTP_STATUS_SUCCESS &&
   payload.response
